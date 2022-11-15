f7341f7f9fd2866e0f8b8c03dc6dcec5fd0dab7d
SimJhaymez <chimobi.ndidiaha@gmail.com>
2022-11-15 10:03:02 -0800

    Simple shell

--- Eshell/main.c
+++ Eshell/main.c
@@ -4,24 +4,74 @@
 int main(int ac, char **argv){
 
 	char *prompt = "(Eshell) $ ";
-	char *lineptr;
+	char *lineptr = NULL, *lineptr_copy = NULL;
 	size_t n = 0;
+	ssize_t nchars_read;
+	const char *delim = "\n";
+	int num_tokens = 0;
+	char *token;
+	int i;
 
-	/*declaring void variables */
-   (void)ac; (void)argv;
+	/* declaring void variables */
+	(void)ac; (void)argv;
 
-	/* create an infinite loop */
-     while (1){
 
-	printf("%s", prompt);
-	getline(&lineptr, &n, stdin);
-	printf("%s\n", lineptr);
+	/* create an infirnte loop*/
+	while (1){
+          printf("%s", prompt);
+	  nchars_read = getline(&lineptr, &n, stdin);
+	  /* check if the getline function failed or reached EOF or gthe user CRTL + D */
+	  if (nchars_read == -1){
+	      printf("Exiting shell....\n");
+	      return (-1);
+	  }
+
+	/* allocate space for a copy of the lineptr */
+	lineptr_copy = malloc(sizeof(char) * nchars_read);
+	if (lineptr_copy == NULL){
+		perror("tsh: memeory allocated error");
+		
+		return (-1);
+	}
 	
+	/* copy lineptr to lineptr_copy */
+	strcpy(lineptr_copy, lineptr);
 
-	/* free up allocated memory*/
-	free(lineptr);
-      }
+	/* split the string (Lineptr) into an array of words */
+	/* calculate the total number of tokens */
+		token = strtok(lineptr, delim);
+
+		while (token != NULL){
+			num_tokens++;
+			token = strtok(NULL, delim);
+		}
+		num_tokens++;
+		
+		/* Allocate space to nold the array of strings */
+		argv = malloc(sizeof(char *) * num_tokens);
 
 
-    return(0);
+		/* Store each token in the argv array */
+		token = strtok(lineptr_copy, delim);
+
+		for (i = 0; token != NULL; i++){
+		    argv[i] = malloc(sizeof(char) * strlen(token));
+     		    strcpy(argv[i], token);
+
+   		    token = strtok(NULL, delim);
+		}
+
+		argv [1] = NULL;
+
+
+
+	        printf("%s\n", lineptr);
+
+	  
+	        /* free up allocated memory */
+	        free(lineptr);
+        }
+
+
+   return(0);
 }
